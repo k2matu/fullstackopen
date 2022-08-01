acdb511fa088aa269a63c87a8207e800149b28c8
k2matu <kstt90@gmail.com>
2022-08-02 00:19:02 +0300

    bloglist 4.21 donem

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,40 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const Course = (props) => {
-	const {name, parts} = props.course
+	const { name, parts } = props.course;
 	return (
-	  <div>
-		<Header course={name} />
-		<Content parts={parts} />
-		<Total total={parts}/>
-	  </div>
-	)
-  }
-  const Header = ({ course }) => <h1>{course}</h1>
-  
-  const Total = ({total}) => {
+		<div>
+			<Header course={name} />
+			<Content parts={parts} />
+			<Total total={parts} />
+		</div>
+	);
+};
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ total }) => {
 	const Amount = () => {
-	  const totalAmount = total.reduce(function(sum, parts) {
-		return sum + parts.exercises
-		}, 0)
-		return (totalAmount)
-	  }
-	return (
-	<b> total of exercises {Amount()} </b>
-	)
-  }
-  
-  const Part = ({ part }) => 
+		const totalAmount = total.reduce(function (sum, parts) {
+			return sum + parts.exercises;
+		}, 0);
+		return totalAmount;
+	};
+	return <b> total of exercises {Amount()} </b>;
+};
+
+const Part = ({ part }) => (
 	<p>
-	  {part.name} {part.exercises}
+		{part.name} {part.exercises}
 	</p>
-  
-  const Content = ({ parts }) => {
+);
+
+const Content = ({ parts }) => {
 	return (
-	  <>
-		{parts.map(part => <Part key={part.id} part={part} />)}
-	  </>
-	)
-  }
-  
-  export default Course
+		<>
+			{parts.map((part) => (
+				<Part key={part.id} part={part} />
+			))}
+		</>
+	);
+};
+
+export default Course;
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -4,6 +4,8 @@ const tokenExtractor = (request, response, next) => {
 	const authorization = request.get("authorization");
 	if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
 		request.token = authorization.substring(7);
+	} else {
+		response.status(401).json({ error: "token missing" });
 	}
 
 	next();
